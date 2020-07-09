1e897ad6f4493e0f49b0073a34c0ef7304003ad5
Nick High <nick@i386.io>
2020-07-09 15:23:28 -0400

    add build script
    Signed-off-by: Nick High <nick@i386.io>

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+TOP=$(pwd)
+echo "TOP = $TOP"
+
+rm -r 3rdparty/install
+mkdir 3rdparty/install
+
+echo "Build OpenSSL"
+cd 3rdparty/openssl
+./Configure linux-mips32 no-async --prefix="$TOP/3rdparty/install"
+make clean
+make -j$(nproc) CC="$TOP/toolchain/bin/mips-linux-uclibc-gnu-gcc"
+make -j$(nproc) install
+cd ../../
+
+echo "Build live555"
+cd 3rdparty/live
+make distclean
+./genMakefiles wyze
+make -j$(nproc)
+make install
+cd ../../
+
+echo "Build wyzedom"
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make
+cd ..
