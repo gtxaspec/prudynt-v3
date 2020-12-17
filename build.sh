1bc3e53ee4ef0b07097b662ace48ef0a3d0e4e8e
Nick High <nick@i386.io>
2020-12-17 16:28:43 -0500

    build for glibc systems
    Signed-off-by: Nick High <nick@i386.io>

--- build.sh
+++ build.sh
@@ -11,22 +11,24 @@ echo "Build OpenSSL"
 cd 3rdparty/openssl
 ./Configure linux-mips32 no-async --prefix="$TOP/3rdparty/install"
 make clean
-make -j$(nproc) CC="$TOP/toolchain/bin/mips-linux-uclibc-gnu-gcc"
+make -j$(nproc) CC="${PRUDYNT_CROSS}gcc"
 make -j$(nproc) install
 cd ../../
 
 echo "Build live555"
 cd 3rdparty/live
-make distclean
+if [[ -f Makefile ]]; then
+    make distclean
+fi
 ./genMakefiles wyze
-PRUDYNT_ROOT="$TOP" make -j$(nproc)
-PRUDYNT_ROOT="$TOP" make install
+PRUDYNT_ROOT="${TOP}" PRUDYNT_CROSS="${PRUDYNT_CROSS}" make -j$(nproc)
+PRUDYNT_ROOT="${TOP}" PRUDYNT_CROSS="${PRUDYNT_CROSS}" make install
 cd ../../
 
 echo "Build prudynt"
 rm -rf build
 mkdir build
 cd build
-cmake ..
+cmake -DPRUDYNT_CROSS="${PRUDYNT_CROSS}" ..
 make
 cd ..
