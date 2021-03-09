daa78ad319121aa1afe351bb4ab2459df1d987ea
Nick High <nick@i386.io>
2021-03-09 10:32:46 -0500

    connect RTSP framesource and logger
    Signed-off-by: Nick High <nick@i386.io>

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "MsgChannel.hpp"
 #include "Encoder.hpp"
 #include "FrameWaterfall.hpp"
+#include "RTSP.hpp"
 #include "Logger.hpp"
 
 template <class T> void start_component(T c) {
@@ -12,15 +13,22 @@ template <class T> void start_component(T c) {
 
 Logger logger;
 Encoder enc;
+RTSP rtsp;
 
 int main(int argc, const char *argv[]) {
     //Logging channels
     logger.connect(&enc);
+    logger.connect(&rtsp);
+
+    //Encoder -> RTSP for broadcast
+    enc.connect_sink(&rtsp);
 
     std::thread log_thread(start_component<Logger>, logger);
     std::thread enc_thread(start_component<Encoder>, enc);
+    std::thread rtsp_thread(start_component<RTSP>, rtsp);
 
     log_thread.join();
     enc_thread.join();
+    rtsp_thread.join();
     return 0;
 }
