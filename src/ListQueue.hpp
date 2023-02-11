74f9373d874433d46b6106282883d286c075130a
Nick High <nick@i386.io>
2023-02-11 14:46:36 -0500

    ListQueue: make locking
    Not sure what I was thinking; the previous version was thread
    unsafe.
    
    Signed-off-by: Nick High <nick@i386.io>

--- src/ListQueue.hpp
+++ src/ListQueue.hpp
@@ -2,6 +2,7 @@
 #define ListQueue_hpp
 
 #include <atomic>
+#include <iostream>
 #include <condition_variable>
 #include <mutex>
 
@@ -17,23 +18,29 @@ public:
         qn->next = NULL;
         head = qn;
         tail = qn;
+        size = 0;
     }
 
     T wait_read() {
-        ListQNode<T> *phead = head;
-        ListQNode<T> *pnext;
-
-        std::unique_lock<std::mutex> lck(cv_mtx);
-        while ((pnext = phead->next) == NULL) {
+        std::unique_lock<std::mutex> lck(lq_mutex);
+        if (head->next == NULL) {
             write_cv.wait(lck);
         }
 
+        ListQNode<T> *phead = head;
+        ListQNode<T> *pnext = head->next;
+        if (pnext == NULL) {
+            //Shouldn't be possible.
+            std::cout << "No data in ListQueue after waiting on cv" << std::endl;
+        }
         head = pnext;
+        --size;
         delete phead;
         return pnext->data;
     }
 
     bool read(T* out) {
+        std::unique_lock<std::mutex> lck(lq_mutex);
         ListQNode<T> *phead = head;
         ListQNode<T> *pnext = head->next;
         if (pnext == NULL) {
@@ -41,24 +48,31 @@ public:
         }
         *out = pnext->data;
         head = pnext;
+        --size;
         delete phead;
         return true;
     }
 
     void write(T msg) {
-        std::unique_lock<std::mutex> lck(cv_mtx);
+        std::unique_lock<std::mutex> lck(lq_mutex);
         ListQNode<T> *add = new ListQNode<T>;
         add->data = msg;
         add->next = NULL;
         tail->next = add;
         tail = add;
+        ++size;
         write_cv.notify_all();
     }
 
+    int queue_size() {
+        return size;
+    }
+
 private:
-    std::atomic<ListQNode<T>*> head;
+    ListQNode<T>* head;
     ListQNode<T>* tail;
-    std::mutex cv_mtx;
+    std::mutex lq_mutex;
+    std::atomic<int> size;
     std::condition_variable write_cv;
 };
 
