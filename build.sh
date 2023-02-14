a6b69a1d04d1cb63ce284343f628921f9038ba36
Nick High <nick@i386.io>
2023-02-14 02:20:29 -0500

    update openssl to 1.1.1t
    Signed-off-by: Nick High <nick@i386.io>

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ mkdir 3rdparty/install
 echo "Build OpenSSL"
 cd 3rdparty
 rm -rf openssl
-if [[ ! -f openssl-1.1.1s.tar.gz ]]; then
-    wget 'https://www.openssl.org/source/openssl-1.1.1s.tar.gz'
+if [[ ! -f openssl-1.1.1t.tar.gz ]]; then
+    wget 'https://www.openssl.org/source/openssl-1.1.1t.tar.gz'
 fi
-tar xvf openssl-1.1.1s.tar.gz
-mv openssl-1.1.1s openssl
+tar xvf openssl-1.1.1t.tar.gz
+mv openssl-1.1.1t openssl
 cd openssl
 ./Configure linux-mips32 no-async --prefix="$TOP/3rdparty/install"
 make clean
